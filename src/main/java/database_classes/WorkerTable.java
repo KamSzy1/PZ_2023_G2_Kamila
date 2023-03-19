cb62b162dd96fa099b4c6674ce86790dd99970c4
Kamszy1 <kam.szydelko11@gmail.com>
2023-03-19 20:24:23 +0100

    Dodanie klas i folderów

---
+++ src/main/java/database_classes/WorkerTable.java
@@ -0,0 +1,9 @@
+package database_classes;
+
+public class WorkerTable {
+    // Tutaj doda się kod do tabeli z pracownikami
+    // Klasa została utworzona testowo, nazwę można podmienić, ale proszę o:
+    // Zasada: jedna tabela == jedna klasa -> ale to jeszcze przedyskutuję z Patrykiem
+    // Jeśli clasa odnosi się do tabeli w bazie danych to proszę o dodawanie "Table" na końcu
+    // Piszemy: "WorkerTable", nie "Worker_Table", ani "worker_table" !
+}
---
+++ src/test/java/database_classes/WorkerTableTest.java
@@ -0,0 +1,7 @@
+package database_classes;
+
+public class WorkerTableTest {
+    // Klasa do testowania WorkerTable
+    // Utworzona testowo, można edytować
+    // Pilnujemy tylko schematu folderów i plików z "src/main/java"
+}
