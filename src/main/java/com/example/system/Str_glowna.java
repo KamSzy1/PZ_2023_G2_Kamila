34cdc2d9f3020b15f5aa0259541457c7b5318320
11Lidia11 <lidiapacyna@gmail.com>
2023-03-20 22:30:52 +0100

    Gui na razie przykładowe, póki co nie robiłam do nich wyglądu sprawdzcie jednie proszę czy wam takie pasuje i co trzeba ewentualnie zmienić.

---
+++ src/main/java/com/example/system/Str_glowna.java
@@ -0,0 +1,7 @@
+package com.example.system;
+
+import javafx.scene.image.Image;
+
+public class Str_glowna {
+
+}
---
+++ src/main/java/com/example/system/Uzytkownik.java
@@ -0,0 +1,7 @@
+package com.example.system;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+public class Uzytkownik {
+}
