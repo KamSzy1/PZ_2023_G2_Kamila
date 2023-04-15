21a4b24b176dbe69a0e7839679575c473d3e5590
Kamszy1 <kam.szydelko11@gmail.com>
2023-04-15 18:11:18 +0200

    GUI

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -21,13 +21,21 @@ public class MainController {
     @FXML
     private GridPane gridLogin;
     @FXML
+    private GridPane gridToken;
+    @FXML
     private GridPane gridRegistration;
     @FXML
     private TextField emailField;
     @FXML
     private PasswordField passwordField;
     @FXML
+    private PasswordField repeatPasswordField;
+    @FXML
+    private TextField tokenField;
+    @FXML
     private Button loginButton;
+    @FXML
+    private Button tokenButton;
     @FXML
     private Button registrationButton;
     @FXML
@@ -35,29 +43,47 @@ public class MainController {
     @FXML
     private Label wrongLogin;
     @FXML
+    private Label wrongRegistration;
+    @FXML
+    private Label wrongTokenLabel;
+    @FXML
     private Label logRegLabel;
 
-    public void buttonsHandler(ActionEvent event) throws IOException{
+    public void buttonsHandler(ActionEvent event) throws IOException {
         //Logowanie
-        if(event.getSource() == loginButton){
+        if (event.getSource() == loginButton) {
             login();
         }
 
-        //Przejście do gridu z panelem rejestracji
-        if(event.getSource() == regChangeButton){
-            wrongLogin.setText("");
+        //Przejście do gridu z panelem tokenu
+        if (event.getSource() == regChangeButton) {
+            backButton.setVisible(true);
             logRegLabel.setText("Rejestracja");
-            gridRegistration.toFront();
+            gridToken.toFront();
         }
 
-        //Rejestracja użytkownika
-        if(event.getSource() == registrationButton){
+        //Sprawdzenie tokenu i przejście dalej - na razie na sztywno
+        if (event.getSource() == tokenButton) {
+            if(tokenField.getText().equals("1234")){
+                gridRegistration.toFront();
+            }else{
+                wrongTokenLabel.setText("Nieprawidłowy token");
+            }
+
+        }
+
+        //
+        if (event.getSource() == registrationButton) {
+
             registration();
+
         }
 
         //Powrót z rejestracji do loginu
-        if(event.getSource() == backButton){
+        if (event.getSource() == backButton) {
             logRegLabel.setText("Logowanie");
+            backButton.setVisible(false);
+            wrongLogin.setText("");
             gridLogin.toFront();
         }
 
@@ -90,53 +116,43 @@ public class MainController {
     //Metoda do rejestrowania użytkowników
     public void registration() throws IOException {
 
-//        //To jest po to, aby zmienić scene/wyświetlany panel
-//        Main main = new Main();
-//
-//        //Pobranie wszystkich danych
-//        String name, surname, address, zipCode, phoneNumber, place, email, password, repeat_password;
-//
-//        name = nameField.getText();
-//        surname = surnameField.getText();
-//        address = addressField.getText();
-//        zipCode = zipCodeField.getText();
-//        place = placeField.getText();
-//        phoneNumber = phoneNumberField.getText();
-//        email = emailField.getText();
-//        password = passwordField.getText();
-//        repeat_password = repeatPasswordField.getText();
-//
-//        boolean everythingOk = false;
-//        //Te metody znajdują się wklasie ValidateData w folderze "other"
-//        try {
-//            ValidateData.goodName(name);
-//            ValidateData.goodSurname(surname);
-//            ValidateData.goodAddress(address);
-//            ValidateData.goodEmail(email);
-//            ValidateData.samePassword(password, repeat_password);
-//            ValidateData.goodZipCode(zipCode);
-//            ValidateData.goodPlace(place);
-//            ValidateData.goodPhoneNumber(phoneNumber);
-//            everythingOk = true;
-//
-//            wrongRegistration.setText("Zarejestrowano pomyślnie");
-//        } catch (Exception e) {
-//            wrongRegistration.setText(e.getMessage());
-//        }
-//
-//        //Jeśli wszyskie dane są poprawne, to doda się do bazy danych wszystko
-//        if (everythingOk) {
-//
-//            password = PasswordHash.hashedPassword(password);
-//            System.out.println(password);
-//
-//            //Tutaj powinien pojawić się kod dodawnia do bazy danych
-//
-//
-//            //Po wykonaniu się wszystkiego zmieni się panel [na portzeby testów na razie kod jest w komentarzu]
-//            //main.changeScene("/main.fxml");
-//
-//        }
+        //To jest po to, aby zmienić scene/wyświetlany panel
+        StageChanger stageChanger = new StageChanger();
+
+        //Pobranie wszystkich danych
+        String email, password, repeat_password;
+
+        email = emailField.getText();
+        password = passwordField.getText();
+        repeat_password = repeatPasswordField.getText();
+
+        boolean everythingOk = false;
+
+        //Te metody znajdują się wklasie ValidateData w folderze "other"
+        try {
+            ValidateData.goodEmail(email);
+            ValidateData.samePassword(password, repeat_password);
+            everythingOk = true;
+
+            //Linijkę poniżej można usunąć, jak się zrobi rejesrtację do końca
+            wrongRegistration.setText("Zarejestrowano pomyślnie");
+        } catch (Exception e) {
+            wrongRegistration.setText(e.getMessage());
+        }
+
+        //Jeśli wszyskie dane są poprawne, to doda się do bazy danych wszystko
+        if (everythingOk) {
+
+            password = PasswordHash.hashedPassword(password);
+            System.out.println(password);
+
+            //Tutaj powinien pojawić się kod dodawnia do bazy danych
+
+
+            //Po wykonaniu się wszystkiego zmieni się panel [na portzeby testów na razie kod jest w komentarzu]
+            //stageChanger.changeScene("/main.fxml");
+
+        }
 
     }
 }
