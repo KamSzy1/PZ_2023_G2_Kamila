8b55b485b5efdd5900a75a75febd86d24ec8e078
11Lidia11 <lidiapacyna@gmail.com>
2023-05-09 19:57:38 +0200

    Poprawki

--- src/test/java/controllers/MainControllerTest.java
+++ src/test/java/controllers/MainControllerTest.java
@@ -1,11 +1,26 @@
 package controllers;
-
-import org.junit.jupiter.api.Test;
+import database_classes.UsersTable;
 
 public class MainControllerTest {
 
+    UsersTable expectedUser = new UsersTable();
+        expectedUser.setName("Lidia");
+        expectedUser.setSurname("Pacyna");
+        expectedUser.setLoginIdUser(1);
 
+    MainController login = new MainController();
 
+    // Act
+    UsersTable actualUser = login.login("email2", "haslo2");
 
+    // Assert
+    assertNotNull(actualUser)
 
+    void assertEquals(expectedUser.getLoginName(), actualUser.getName());
+    void assertEquals(expectedUser.getLoginSurname(), actualUser.getSurname());
+    void assertEquals(expectedUser.getLoginIdUser(), actualUser.getLoginIdUser());
 }
+
+
+
+
