a613d67997ce0ca8f4c678ed0e71ad3de8ceff44
Patryk <82804262+PatrykWyczawski@users.noreply.github.com>
2023-06-19 16:54:34 +0200

    masakra

--- src/test/java/database_classes/StatusesTableTest.java
+++ src/test/java/database_classes/StatusesTableTest.java
@@ -1,4 +1,42 @@
 package database_classes;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class StatusesTableTest {
+
+    StatusesTable statusesTable = new StatusesTable();
+
+    @Test
+    void testGetIdStatus() {
+        int expectedIdStatus = 1;
+        statusesTable.setIdStatus(expectedIdStatus);
+        int actualIdStatus = statusesTable.getIdStatus();
+        assertEquals(expectedIdStatus, actualIdStatus);
+    }
+
+    @Test
+    void testGetName() {
+        String expectedName = "Po terminie";
+        statusesTable.setName(expectedName);
+        String actualName = statusesTable.getName();
+        assertEquals(expectedName, actualName);
+    }
+
+    @Test
+    void testSetIdStatus() {
+        int idStatus = 1;
+        statusesTable.setIdStatus(idStatus);
+        int actualIdStatus = statusesTable.getIdStatus();
+        assertEquals(idStatus, actualIdStatus);
+    }
+
+    @Test
+    void testSetName() {
+        String name = "Po terminie";
+        statusesTable.setName(name);
+        String actualName = statusesTable.getName();
+        assertEquals(name, actualName);
+    }
 }
